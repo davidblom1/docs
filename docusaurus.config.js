745fd65f59aa84c14cfa142379bed58c93d90613
Alex Colello <alexlcolello@gmail.com>
2021-01-28 18:35:08 -0800

    More cleanup

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -94,13 +94,13 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
-            'https://github.com/uscmakers/docs/edit/master',
+            'https://github.com/uscmakers/docs/edit/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/uscmakers/docs/edit/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
